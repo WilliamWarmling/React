3811180da3d9e7b492e982192473e827d1173f67
Pedro Nieto <pedronieto.2005@gmail.com>
2023-10-03 21:39:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import videogame from './videogame.png';
+import { useState } from 'react';
 
 function App() {
+  const [numero, setNumero] = useState(0);
+
+  function aumentarNumero() {
+    setNumero(numero + 1);
+  }
+
+  function diminuirNumero() {
+    setNumero(numero - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Projeto final - Ajudando Gamers</h1>
+      <img src={videogame} alt="imagem" />
+
+      <p className='p'>Nosso projeto tem como base ajudar os jogadores de diversos jogos. Através de dicas, tutoriais, explicações e truques, será possível facilitar muito a vida de um jogador que está iniciando uma nova jornada em seu game novo!!!</p>
+      <hr />
+
+      <div>
+        <button onClick={diminuirNumero}>Fase anterior</button>
+        <p className='fase'>Fase: {numero}</p>
+        <button onClick={aumentarNumero}>Próxima fase</button>
+      </div>
     </div>
   );
 }
